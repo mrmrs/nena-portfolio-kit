fb2f1d265234da62fdcd6f91d569d1d54580235e
mrmrs <hi@mrmrs.cc>
2017-12-04 16:28:11 +0100

    Initial components and sample pages

---
+++ Index.jsx
@@ -0,0 +1,34 @@
+<SiteContainer pt={4}>
+<Flex wrap>
+<Box px={4} pb={5} w={[1, 1/4]}>
+  <Logo />
+  <BlockLink mt={2} children="About Me" />
+  <SectionTitle mt={4} children='Projects' />
+  <TextLink children='Vector Graphics for Open Source Apps' />
+  <TextLink children='Sandstorm Website' />
+  <TextLink children='Grain of Rice Restaurant Menu' />
+  <TextLink children='Sandstorm Community Illustrations & Graphics' />
+   <TextLink  children='A Field Guide to Bicycles' />
+    <TextLink mt={5} children='Sandstorm website' />
+     <TextLink children='Sandstorm App Market' />
+  
+</Box>
+<Box w={[1,3/4]} px={3}>
+<Flex wrap>
+    <Card title='Vector Graphics for Open Source Apps' />
+    <Card title='Sandstorm Website'
+    image='https://pro2-bar-s3-cdn-cf2.myportfolio.com/297cb0cf023a849662c66db236fb4fa7/3852390e88f9c0695f156b8613d1e18ffdb3db32b740923a28b518bb46d03c7b20316899bc93f97e_rwc_103x207x865x678x865.png'
+     />
+    <Card title='Grain of Rice Restaurant Menu'
+    image='https://pro2-bar-s3-cdn-cf4.myportfolio.com/297cb0cf023a849662c66db236fb4fa7/5063f0a7a7f7d4c0631ed0054723264f4a196953cc8e0b5621417c016529bafa156ab97e6f5b773d_carw_202x158x1280.jpg?h=a003ee36ca7c362a3bc8e27bb9bed670&url=aHR0cHM6Ly9taXItczMtY2RuLWNmLmJlaGFuY2UubmV0L3Byb2plY3RzL29yaWdpbmFsL2VlMDc2ZTQ2MTY2MzYzLlkzSnZjQ3c1Tnprc056WTJMRFV6TERFMC5qcGc=' />
+ <Card title='Sandstorm Community Illustrations & Graphics'
+    image='https://pro2-bar-s3-cdn-cf4.myportfolio.com/297cb0cf023a849662c66db236fb4fa7/f47a1a5a-1c83-47e5-b3ac-ec69808740df_rwc_619x73x682x534x1280.png?h=4a880e9a1e51fc869bb58a12f888cec1' />
+     <Card title='A Field Guide to Bicycles'
+    image='https://pro2-bar-s3-cdn-cf6.myportfolio.com/297cb0cf023a849662c66db236fb4fa7/046e7aa99d01e5c838800185804d09606d50b929a2a74765bf8c42c0af1614ded617b110df387d91_carw_202x158x1280.png?h=9da48e3254058754cf877871fdac946e&url=aHR0cHM6Ly9taXItczMtY2RuLWNmLmJlaGFuY2UubmV0L3Byb2plY3RzL29yaWdpbmFsLzYwZjVhMzM0NzYxMzc5LlkzSnZjQ3c0TURBc05qSTFMREFzTUEucG5n' />
+</Flex>
+</Box>
+</Flex>
+</SiteContainer>
+<Footer p={4} color='gray'>
+  Nena Nguyen | Copyright 2016
+</Footer>
---
+++ Project.jsx
@@ -0,0 +1,22 @@
+<SiteContainer pt={4}>
+<Flex wrap>
+<Box px={4} w={[1, 1/4]}>
+  <Logo />
+  <BlockLink mt={2} children="About Me" />
+  <SectionTitle mt={4} children='Projects' />
+  <TextLink children='Vector Graphics for Open Source Apps' />
+  <TextLink children='Sandstorm Website' />
+  <TextLink children='Grain of Rice Restaurant Menu' />
+  <TextLink children='Sandstorm Community Illustrations & Graphics' />
+   <TextLink children='A Field Guide to Bicycles' />
+  
+</Box>
+<Box w={[1,3/4]} px={3}>
+  <ProjectTitle children="Vector Graphics for Open Source Apps" />
+  <ProjectText children='
+These colourful vector logos & graphics were designed for a selected number of open source apps on the Sandstorm App Market.' />
+
+<Image src='https://pro2-bar-s3-cdn-cf3.myportfolio.com/297cb0cf023a849662c66db236fb4fa7/e4ddc237e0baff084be0062c_rw_1920.png?h=b1a2d30207ba233fb9bca3791a26f707' />
+</Box>
+</Flex>
+</SiteContainer>
